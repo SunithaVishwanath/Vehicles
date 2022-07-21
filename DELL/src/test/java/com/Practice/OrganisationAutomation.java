31fa644d83fef172aaf81187b92dabd9ba468ffa
Nithin <Nithin@DESKTOP-TKNJ4V5>
2022-07-21 17:57:40 +0530

    commit1

---
+++ DELL/src/test/java/com/Practice/OrganisationAutomation.java
@@ -0,0 +1,13 @@
+package com.Practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OrganisationAutomation {
+	@Test
+	public void organisationName()
+	{
+		Reporter.log("hello");
+	}
+	
+}
